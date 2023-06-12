2833235e1d5f54e75207cf5e98d50be01500f3ae
Naisalie <naisaliew@gmail.com>
2023-06-12 18:20:39 -0400

    Add event listeners to all game buttons

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+const gameButtons = Array.from(document.querySelectorAll("button"));
+
+//Add an event listener that calls playRound with the clicked button
+gameButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound((computer = getComputerChoice()), button.textContent);
+  });
+});
+
 //Randomly return "Rock", "Paper", or "Scissors"
 //Make the item returned based on a random number between 1 and 3 and rounded to the nearest whole number
 let getComputerChoice = () => {
@@ -20,17 +29,21 @@ let getComputerChoice = () => {
 };
 
 //Play a single round of Rock, Paper Scissors
-let playRound = (computer = getComputerChoice(), player = prompt("Choose item!")) => {
+let playRound = (computer, player) => {
   //Change to title case
   player = player.slice(0, 1).toUpperCase() + player.slice(1).toLowerCase();
   if ((computer === "Rock" && player === "Paper") || (computer === "Paper" && player === "Scissors") || (computer === "Scissors" && player === "Rock")) {
-    return "You Win";
+    // return "You Win";
+    console.log("win");
   } else if ((computer === "Rock" && player === "Scissors") || (computer === "Paper" && player === "Rock") || (computer === "Scissors" && player === "Paper")) {
-    return "Computer Wins";
+    // return "Computer Wins";
+    console.log("lose");
   } else if (computer === player) {
-    return "Tie";
+    // return "Tie";
+    console.log("tie");
   } else {
-    return `Not a possible outcome.`;
+    // return `Not a possible outcome.`;
+    console.log("n/a");
   }
 };
 
